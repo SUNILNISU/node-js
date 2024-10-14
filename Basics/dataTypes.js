992c19df06372757ff805fcb5d9103dea2162099
SUNILNISU <155054622+SUNILNISU@users.noreply.github.com>
2024-10-14 10:42:31 +0000

    container config done

---
+++ Basics/dataTypes.js
@@ -0,0 +1,12 @@
+//number=2 to power 53
+//bignit
+//string=""
+//boolean=true/false
+//null=Standalone value
+//undefined
+//symbol
+
+let age=20
+let name="sunil"
+let result=false
+console.table([typeof age,typeof name,typeof result,typeof undefined,typeof null])
