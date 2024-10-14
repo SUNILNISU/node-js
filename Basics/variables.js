e3fdf17b08548f62e1173637179ac6814af20c4f
SUNILNISU <155054622+SUNILNISU@users.noreply.github.com>
2024-10-14 10:23:07 +0000

    container config done

---
+++ Basics/variables.js
@@ -0,0 +1,18 @@
+const accountID=532629704996  //Use this for constant
+let accountEmail="mathphysics@gamil.com" //use this for variables
+var accountPassword="Yadav@12"
+accountCity="Prayagraj" //This is also one of the way to initialise the variable in the java script.
+let accountState  //This will return the undefined value
+ 
+// accountID=235467
+// console.log(accountID)  This will give error because const  value cannot change.
+/*
+Avoid use of the var because of issue in block scope
+ans functional scope.
+*/
+
+accountEmail="abcderg@gmail.com"
+accountPassword="2345678"
+accountCity="Delhi"
+
+console.table([accountEmail,accountID,accountPassword,accountCity,accountState])
